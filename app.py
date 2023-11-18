956ad2f16080105f072ab283adbbf6ac3c76a6bb
globalnebula <kunalachintya@gmail.com>
2023-11-18 19:41:33 +0530

    Auth functionality and improved Frontend

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, Request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////base.db"
-
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class User(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
@@ -16,9 +18,45 @@ class Questions(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(250), nullable = False, unique = True)
 
-@app.route('/')
-def index():
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            if check_password_hash(user.password, password):
+                return 'Login successful!'
+            else:
+                return 'Incorrect password!'
+        else:
+            return 'User not found!'
+    
     return render_template('login.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            return 'Username already exists!'
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return "Registered Successfully!"
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(port="1908",debug=True)
