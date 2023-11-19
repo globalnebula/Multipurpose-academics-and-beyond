68705ed07957d13d71eb833f7cfbcaaccc20cac5
globalnebula <kunalachintya@gmail.com>
2023-11-19 22:51:16 +0530

    improved error handling

--- app.py
+++ app.py
@@ -1,45 +1,57 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
+from flask_login import LoginManager, login_user, UserMixin, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.secret_key = 'AJ94c36aUhnp5ACooY7X6kIc4qgVubLY'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class User(db.Model):
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable = False, unique = True)
-    email = db.Column(db.String(50), nullable = False, unique = True)
-    password = db.Column(db.String(128), nullable = False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
 
 class Questions(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(250), nullable = False, unique = True)
+    question = db.Column(db.String(250), nullable=False, unique=True)
 
 with app.app_context():
     db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-
         user = User.query.filter_by(username=username).first()
 
-        if user:
-            if check_password_hash(user.password, password):
-                return 'Login successful!'
-            else:
-                return 'Incorrect password!'
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return render_template('home.html')
         else:
-            return 'User not found!'
-    
+            flash('Incorrect username or password!')
+            return render_template('login.html')
+
     return render_template('login.html')
 
-@app.route('/register', methods=['GET','POST'])
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -49,14 +61,15 @@ def register():
         existing_user = User.query.filter_by(username=username).first()
 
         if existing_user:
-            return 'Username already exists!'
+            flash('User already exists!')
+            return redirect(url_for('login'))
+
         hashed_password = generate_password_hash(password)
         new_user = User(username=username, email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        
-        return "Registered Successfully!"
+
     return render_template('register.html')
 
 if __name__ == "__main__":
-    app.run(port="1908",debug=True)
+    app.run(port="1908", debug=True)
